aa7e96720b7f826bbb7a6a8cc33c01ae3a025750
Siyi Gu <nodnightofday@gmail.com>
2016-02-26 17:51:51 -0500

    temp commit

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-var express = require('express')
+var express = require('express');
 var app = express();
 app.use(express.static('public'));
 
@@ -6,6 +6,10 @@ var db = require('./db.js');
 
 app.get('/update', function (req, res) {
 	db.update();
+});
+
+app.get('/connect', function(req, res){
+	db.connect();
 });
 
 app.listen(3000, function () {
--- db.js
+++ db.js
@@ -1,59 +1,30 @@
 var Db = function () {};
 var AWS = require('aws-sdk');
-var fs = require("fs");
 AWS.config.update({region:'us-west-2'});
 
-Db.prototype.connect = function () {
-	var dynamodb = new AWS.DynamoDB();
+var fs = require("fs");
+var md5 = require("blueimp-md5");
 
-	var params = {
-		TableName : "Movies",
-		KeySchema: [       
-			{ AttributeName: "year", KeyType: "HASH"},  //Partition key
-			{ AttributeName: "title", KeyType: "RANGE" }  //Sort key
-		],
-		AttributeDefinitions: [       
-			{ AttributeName: "year", AttributeType: "N" },
-			{ AttributeName: "title", AttributeType: "S" }
-		],
-		ProvisionedThroughput: {       
-			ReadCapacityUnits: 10, 
-			WriteCapacityUnits: 10
-		}
-	};
+Db.prototype.update = function(information){
+	var toUpdate = {};
+	var toUpdate.name = information.name;
+	var toUpdate.email = information.email;
+	var toUpdate.office = information.office;
+	var toUpdate.submissionTime = Date.now();
+	var toUpdate.entryId = md5(submissionTime);
+	var toUpdate.fileName = entryId + ".pdf";
+	console.log(toUpdate);
 
-	dynamodb.createTable(params, function(err, data) {
-		if (err) {
-			console.error("Unable to create table. Error JSON:", JSON.stringify(err, null, 2));
-		} else {
-			console.log("Created table. Table description JSON:", JSON.stringify(data, null, 2));
-		}
-	});
-};
+/*
+	var docClient = new AWS.DynamoDB.DocumentClient();
 
-Db.prototype.update = function(){
-var docClient = new AWS.DynamoDB.DocumentClient();
-
-console.log("Importing movies into DynamoDB. Please wait.");
-
-var allMovies = JSON.parse(fs.readFileSync('moviedata.json', 'utf8'));
-allMovies.forEach(function(movie) {
-    var params = {
-        TableName: "Receipe",
-        Item: {
-            "year":  movie.year,
-            "title": movie.title,
-            "info":  movie.info
-        }
-    };
-
+	
     docClient.put(params, function(err, data) {
-       if (err) {
-           console.error("Unable to add movie", movie.title, ". Error JSON:", JSON.stringify(err, null, 2));
-       } else {
-           console.log("PutItem succeeded:", movie.title);
-       }
-    });
-});
+    	if (err) {
+        	console.error("Unable to add movie", movie.title, ". Error JSON:", JSON.stringify(err, null, 2));
+       	} else {
+        	console.log("PutItem succeeded:", movie.title);
+       	}
+	});*/
 };
 module.exports = new Db();

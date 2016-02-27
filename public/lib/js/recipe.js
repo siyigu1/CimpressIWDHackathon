4831e604c44d6d5db962176ae90cd8ee8296f974
Christina Kayastha <ckayastha@vistaprint.com>
2016-02-27 17:24:37 -0500

    recipe init

---
+++ public/lib/js/recipe.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+	$(".pdf-download").on("click", function(){
+		console.log("download pdf started");
+
+		var url = "http://htmlpdfapi.com/api/v1/"
+		var data = {
+			"url": "http://callmenick.com/_development/flexbox-examples/index2.html"
+		}
+
+		var success = function(result){
+			console.log("success")
+			console.log(result)
+		}
+
+		var headers = {
+			"Authentication": "Token 7Kx0RD6hOmeHKrsEG-DpdUX6C0Sw3IKU"
+		}
+
+		$.ajax({
+		  type: "POST",
+		  url: url,
+		  data: data,
+		  success: success,
+		  headers: headers,
+		  crossDomain: true
+		});
+	})
+})
